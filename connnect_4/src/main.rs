a1b41cc8d3caeb76206b77b714de8769a27b7122
Lord-Lelsers <48894200+LelsersLasers@users.noreply.github.com>
2022-08-04 09:44:20 -0700

    drawing board

--- connnect_4/src/main.rs
+++ connnect_4/src/main.rs
@@ -1,3 +1,34 @@
+
+#[derive(Copy, Clone)]
+enum Spot {
+    X,
+    O,
+    Empty,
+}
+impl Spot {
+    fn to_string(&self) -> &'static str {
+        match self {
+            Spot::X => "X",
+            Spot::O => "O",
+            Spot::Empty => " ",
+        }
+    }
+}
+
+fn print_board(board: [[Spot; 7]; 6]) {
+    for row in board.iter() {
+        print!("|");
+        for spot in row.iter() {
+            print!("{}", spot.to_string());
+        }
+        println!("|");
+    }
+    println!("+++++++++");
+    println!("+1234567+");
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut board: [[Spot; 7]; 6] = [[Spot::Empty; 7]; 6];
+    print_board(board);
 }
